7858599420d60a457928dc4f7cff205991dec594
João Victor Trojan <joaovictortrojandepaula@gmail.com>
2026-01-19 09:21:10 -0300

    Update Flask app to use absolute directory paths
    Refactor Flask app to use absolute paths for static and template folders.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 from flask import render_template
-app = Flask(__name__,static_folder="static", template_folder="templates")
+
+Base_dir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__, template_folder=os.path.join(Base_dir, "templates"),static_folder=os.path.join(Base_dir, "static"),)
 
 @app.route("/")
 def HomePage():
     return render_template("HomePage.html")
 if __name__ == "__main__":
-
  app.run()
